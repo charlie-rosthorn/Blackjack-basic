ce740948a295be7e60473af8d37b70776ae57737
Charlie Rosthorn <100390614+charlie-rosthorn@users.noreply.github.com>
2022-03-20 21:13:09 +0000

    Card faces are distinct from values assigned:
    Previously, what was 'drawn' (i.e: printed and displayed) was an int from a deck (e.g: 10). This was fine for most cards, but of course, face-cards (Jack, Queen, King) are represented by something other than their value (e.g: J - 10). This update allows for faceup cards to be shown in a more reflective manner as they would in the game - although the Ace card hasn't become interchangeable with values of either 1 or 11 for certain contexts and is the subject of the next update.

--- blackjack.py
+++ blackjack.py
@@ -1,83 +1,123 @@
 import random
+import functools
 
-# Deck
-deck = [
-    2, 2, 2, 2,
-    3, 3, 3, 3,
-    4, 4, 4, 4,
-    5, 5, 5, 5,
-    6, 6, 6, 6,
-    7, 7, 7, 7,
-    8, 8, 8, 8,
-    9, 9, 9, 9,
-    10, 10, 10, 10,
-    ]
+# Setup
+
+deck = (
+    "2", "2", "2", "2",
+    "3", "3", "3", "3",
+    "4", "4", "4", "4",
+    "5", "5", "5", "5",
+    "6", "6", "6", "6",
+    "7", "7", "7", "7",
+    "8", "8", "8", "8",
+    "9", "9", "9", "9",
+    "10", "10", "10", "10",
+    "J", "J", "J", "J",
+    "Q", "Q", "Q", "Q",
+    "K", "K", "K", "K",
+    "A", "A", "A", "A")
+
+twist_responses = ("Twist", "T", " Twist", " T")
+stick_responses = ("Stick", "S", " Stick", " S")
+
+computer_hand = []
+player_hand = []
 
 # Computer cards
 
-computer_card1 = random.choice(deck)
-print("Computer number: " + str(computer_card1))
-computer_card2 = random.choice(deck)
-print("Computer number: " + str(computer_card2))
-computer = computer_card1 + computer_card2
-print("Total: " + str(computer))
 
-# Initial Draw
+def draw_card_computer():
 
-user_card1 = random.choice(deck)
-print("User number: " + str(user_card1))
-user_card2 = random.choice(deck)
-print("User number: " + str(user_card2))
-user = user_card1 + user_card2
-print("Total: " + str(user))
-
-# Stick or Twist
-
-if user < 21:
-    question1 = input("Would you like to stick or twist?\n")
-    if question1 == "twist":
-        user_card3 = random.choice(deck)
-        print("User number: " + str(user_card3))
-        user = user_card1 + user_card2 + user_card3
-        print("Current Total: " + str(user))
-        if user_card3 + user < 21:
-            question2 = input("Would you like to stick or twist?\n")
-            if question2 == "twist":
-                user_card4 = random.choice(deck)
-                print("User number: " + str(user_card4))
-                user = user_card1 + user_card2 + user_card3 + user_card4
-                print("Current Total: " + str(user))
-                if user_card3 + user_card4 + user < 21:
-                    question3 = input("Would you like to stick or twist?\n")
-                    if question3 == "twist":
-                        user_card5 = random.choice(deck)
-                        print("User number: " + str(user_card5))
-                        user = user_card1 + user_card2 + user_card3 + user_card4 + user_card5
-                        print("Current Total: " + str(user))
+    new_card_key = random.choice(deck)
+    if new_card_key.isdigit():
+        computer_hand.append(int(new_card_key))
+        return new_card_key
+    elif new_card_key == "J":
+        computer_hand.append(10)
+        return new_card_key
+    elif new_card_key == "Q":
+        computer_hand.append(10)
+        return new_card_key
+    elif new_card_key == "K":
+        computer_hand.append(10)
+        return new_card_key
+    else:
+        computer_hand.append(11)
+        return new_card_key
 
 
-if user <= 21 and user > computer:
-    computer_card3 = random.choice(deck)
-    print("Computer number: " + str(computer_card3))
-    computer = computer_card1 + computer_card2 + computer_card3
-    print("Current Total: " + str(computer))
-    if user <= 21 and user > computer:
-        computer_card4 = random.choice(deck)
-        print("Computer number: " + str(computer_card4))
-        computer = computer_card1 + computer_card2 + computer_card3 + computer_card4
-        print("Current Total: " + str(computer))
-        if user <= 21 and user > computer:
-            computer_card5 = random.choice(deck)
-            print("Computer number: " + str(computer_card5))
-            computer = computer_card1 + computer_card2 + computer_card3 + computer_card4 + computer_card5
-            print("Current Total: " + str(computer))
+def draw_card_player():
+
+    new_card_key = random.choice(deck)
+    if new_card_key.isdigit():
+        player_hand.append(int(new_card_key))
+        return new_card_key
+    elif new_card_key == "J":
+        player_hand.append(10)
+        return new_card_key
+    elif new_card_key == "Q":
+        player_hand.append(10)
+        return new_card_key
+    elif new_card_key == "K":
+        player_hand.append(10)
+        return new_card_key
+    else:
+        player_hand.append(11)
+        return new_card_key
 
 
-# Results
+print("Computer:")
+computer_card1 = draw_card_computer()
+print(computer_card1)
+computer_card2 = draw_card_computer()
+print("(facedown card) \nRevealed Total: " + str(computer_card1))
+computer_total = functools.reduce(lambda x, y: x + y, computer_hand)
+print("")
 
-if user <= 21 and (user > computer or computer > 21):
-    print("User wins!")
-elif user > 21 or (computer > user and computer < 22):
-    print("Computer wins!")
+print("Player:")
+player_card1 = draw_card_player()
+print(player_card1)
+player_card2 = draw_card_player()
+print(player_card2)
+player_total = functools.reduce(lambda x, y: x + y, player_hand)
+print("Player Total:  " + str(player_total))
+
+
+if player_total != 21:
+    proceed = 0
+    while proceed == 0:
+        stick_or_twist = input("Would you like to twist or stick?\n").capitalize()
+        if stick_or_twist in twist_responses:
+            another_Card = draw_card_player()
+            player_total = functools.reduce(lambda x, y: x + y, player_hand)
+            print("Player Total:  " + str(player_total))
+            if player_total < 21:
+                proceed = 0
+            if player_total > 21:
+                proceed = 1
+        elif stick_or_twist in stick_responses:
+            proceed += 1
+        else:
+            print("Sorry, please try again.")
+
+if (player_total <= 21) and (player_total >= computer_total):
+    print("Computer's Turn")
+    print("Computer Total:  " + str(computer_total))
+    another_Card = draw_card_computer()
+    print(another_Card)
+    computer_total = functools.reduce(lambda x, y: x + y, computer_hand)
+    if (computer_total < player_total) and (computer_total <= 21):
+        another_Card = draw_card_computer()
+        computer_total = functools.reduce(lambda x, y: x + y, computer_hand)
+        if (computer_total < player_total) and (computer_total <= 21):
+            another_Card = draw_card_computer()
+            computer_total = functools.reduce(lambda x, y: x + y, computer_hand)
+print("Computer total:  " + str(computer_total))
+
+if ((player_total <= 21) and (player_total > computer_total)) or computer_total > 21:
+    print("Player has won!")
+elif ((computer_total > player_total) and (computer_total <= 21)) or player_total > 21:
+    print("Player has lost!")
 else:
     print("Tie!")
